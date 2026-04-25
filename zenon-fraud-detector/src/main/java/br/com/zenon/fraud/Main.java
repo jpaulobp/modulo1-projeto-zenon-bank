a67e21f9b6b03dc0997bef116c1385d8e5a21754
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2026-04-25 08:14:31 -0300

    Tarefa essencial 04:  remoção de inicialização desnecessária

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
@@ -1,16 +1,14 @@
 package br.com.zenon.fraud;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
 	void main() {
 
-		List<Transaction> transactionList = new ArrayList<>();
 		TransactionIngestor ingestor = new TransactionIngestor();
 		//transactionList = ingestor.ingest("PS_20174392719_1491204439457_log.csv");
-		transactionList = ingestor.ingest("paysim_with_bad_data.csv");
+		List<Transaction> transactionList = ingestor.ingest("paysim_with_bad_data.csv");
 		IO.println(transactionList.size());
 		transactionList.forEach(IO::println);
 

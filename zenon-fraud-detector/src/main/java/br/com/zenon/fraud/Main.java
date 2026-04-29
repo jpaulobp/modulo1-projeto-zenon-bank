9452809d8e09144cdeec498747685763f4242483
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2026-04-29 12:10:25 -0300

    Tarefa essencial 05: refatoração

--- zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/fraud/Main.java
@@ -12,10 +12,10 @@ public class Main {
 		FraudAnalyzer analyzer = new FraudAnalyzer(transactionList);
 		IO.println("1. Total de fraudes: " + analyzer.fraudCount());
 		IO.println("2. Top 3 Fraudes de Maior Valor:");
-		analyzer.getHighestValueFrauds(3).forEach(IO::println);
+		analyzer.getHighestValueFrauds(3).forEach(amount -> IO.println("%.2f".formatted(amount)));
 		IO.println("3. Clientes Suspeitos:");
 		analyzer.getTopSuspiciousClients(5).forEach(IO::println);
-		IO.println("4. Prejuízo Total: " + analyzer.getTotalLoss());
+		IO.println("4. Prejuízo Total: %.2f".formatted(analyzer.getTotalLoss()));
 		IO.println("5.  Fraudes por Tipo:");
 		analyzer.getCountByTransactionType().forEach((chave, valor) -> IO.println("- " + chave + ": " + valor));
 
